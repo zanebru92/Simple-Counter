1039e80f33e31415bf0fbe4ef65be8cfef10f240
Bruno Zanella <zanella.bruno@gmail.com>
2022-05-12 16:25:46 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+// set initial count
+let count = 0;
+
+let value = document.querySelector("#value");
+
+let btns = document.querySelectorAll(".btn");
+
+btns.forEach(function (btn) {
+  btn.addEventListener("click", function (e) {
+    let styles = e.currentTarget.classList;
+    if (styles.contains("decrease")) {
+      count--;
+    } else if (styles.contains("increase")){
+        count ++;
+    } else {
+        count = 0;
+    }
+    if(count > 0){
+        value.style.color = 'green';
+    } else if(count < 0){
+        value.style.color = 'red';
+    } else {
+         value.style.color = '#222';
+    }
+    value.textContent = count;
+  });
+});
